72b29122f49e681c67201ed3e5244f0ea8667847
Stephanie Viccari <stephviccari@gmail.com>
2021-03-26 17:48:46 -0400

    wip

---
+++ spec/requests/api/upcoming_events_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe "GET /api/upcoming_events" do
+  it "returns the next upcoming event" do
+    # controller test
+    # get :upcoming_events
+    #
+    # # request specs
+    # get "api/upcoming_events"
+  end
+end
